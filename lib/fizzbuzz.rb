96f9b60a20ac3c8ad31dc38f866751688e8c55fc
Proskurina <yproskurina@yahoo.com>
2015-08-19 12:48:22 +0100

    Add 2 more tests, Fizzbuzz and num

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,13 @@
 def Fizzbuzz(num)
 
-  if num % 3 == 0
+  if num % 15 == 0
+  	return "FizzBuzz"
+  elsif num % 3 == 0
     return "Fizz"
+  elsif num % 5 == 0
+  	return "Buzz"
+  else
+  	return num
   end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,13 @@ describe 'Fizzbuzz' do
   it 'should return Buz when given a number 5' do
     expect(Fizzbuzz(5)).to eq "Buzz"
   end
+
+  it 'should return FizzBuzz when given a number 15' do
+    expect(Fizzbuzz(15)).to eq "FizzBuzz"
+  end 
+
+   it 'should return 4 when given a number 4' do
+    expect(Fizzbuzz(4)).to eq 4
+  end 
+
 end
